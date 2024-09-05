00469ab7fb3bc61a19fa9ad97db60eb334262e03
Dmitry <dimonmini663@gmail.com>
2024-09-05 20:05:36 +0300

    Create alpha-1.0.py

---
+++ old/alpha-1.0.py
@@ -0,0 +1,25 @@
+import time
+
+print("THIS GAME IN ALPHA")
+print("THIS GAME CAN CONTAINS BUGS")
+time.sleep(5)
+print("Welcome to SchoolDay!")
+input('Press enter to play.')
+print("Day 1")
+time.sleep(5)
+print("Do you wanna go to school today?")
+print("Yes or No")
+quest1 = input('')
+if quest1 == "yes":
+  print("Good!")
+  print("Dear user")
+  print("This is end of alpha-1.0")
+  input('')
+elif quest1 == "no":
+  print("Bad Ending")
+  print("You got grounded..")
+  time.sleep(2)
+  print("And you didn't get girlfriend...")
+  time.sleep(5)
+  print("LOSER!!!")
+  time.sleep(0.9)
