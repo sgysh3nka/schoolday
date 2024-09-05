3d3cbcfd2ab71912af5bd6286dfb0eac7968d005
Dmitry <dimonmini663@gmail.com>
2024-09-05 20:24:28 +0300

    Update main.py
    alpha-1.1
    + More stuff
    - Bugs

--- main.py
+++ main.py
@@ -12,9 +12,41 @@ print("Yes or No")
 quest1 = input('')
 if quest1 == "yes":
   print("Good!")
-  print("Dear user")
-  print("This is end of alpha-1.0")
-  input('')
+  input('You see a beautiful girl...')
+  input("She's name's Jessica...")
+  quest2 = input('Do you wanna talk with her? ')
+  if quest2 == "yes":
+    input('You go to her to talk...')
+    input("You say 'hi' to her")
+    input('She says "no"')
+    input('~WHAT THE HELL "NO" IS MEANS???~')
+    input('You: Why no?')
+    input('Jessica: cus u ugly asf')
+    input('You really likes Jessica..')
+    input('You: Bye!')
+    input('Jessica: Bye, bye, ugly boy')
+    quest3 = input('Do you wanna go to GYM to be beautiful? ')
+    if quest3 == "yes":
+      input('You going to GYM..')
+      print("Dear player")
+      print("This is end of alpha-1.1")
+      input('Press enter to exit')
+    elif quest3 == "no":
+      print("GYM Ending (bad)")
+      print("still no gf...?")
+      time.sleep(2)
+      print("LOSER!!!")
+      time.sleep(0.9)
+    else:
+      input('Error')
+  elif quest2 == "no":
+    print("No girlfriend Ending")
+    print("no gf?..")
+    time.sleep(2)
+    print("LOSER!!!")
+    time.sleep(0.9)
+  else:
+    input('Error')
 elif quest1 == "no":
   print("Bad Ending")
   print("You got grounded..")
@@ -23,3 +55,5 @@ elif quest1 == "no":
   time.sleep(5)
   print("LOSER!!!")
   time.sleep(0.9)
+else:
+  input('Error')
